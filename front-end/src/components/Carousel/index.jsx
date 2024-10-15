aba7b917b900e0ba2c1f55b7817a90f66a425861
Rodrigo Donizetti Rocha <rodrigo.rocha9229@gmail.com>
2024-10-15 20:52:53 -0300

    Adicionando imagens e Carousel

--- front-end/src/components/Carousel/index.jsx
+++ front-end/src/components/Carousel/index.jsx
@@ -1,13 +1,14 @@
 
 import Carousel from 'react-bootstrap/Carousel';
 
+
 function CarouselFade() {
     return (
       <Carousel data-bs-theme="dark">
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=f5f5f5"
+            src="img/canario-zaeli.jpg"
             alt="First slide"
           />
           <Carousel.Caption>
