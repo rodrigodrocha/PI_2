0ba6dc4de6486fdddab680019918c10d7231bbf6
Rodrigo Donizetti Rocha <rodrigo.rocha9229@gmail.com>
2024-10-14 23:27:41 -0300

    Front-end desenvolvendo o background do sistema

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola aqui vai meu conteudo</h1>
+      <span>________________________</span>
+    </div>
+  );
+}
+
+export default App;
--- src/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_word():
-    return "<p>Esse vai ser um site de pet<p>"
-
