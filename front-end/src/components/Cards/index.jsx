dcf7059bd51529cf46f706cacc433e186973f9c8
rodrigodrocha <rodrigo.rocha9229@gmail.com>
2024-10-24 11:35:39 -0300

    Colocando icones nos botões e nos links de rede social

--- front-end/src/components/Cards/index.jsx
+++ front-end/src/components/Cards/index.jsx
@@ -3,6 +3,8 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 import "./styles.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 function Cards({imgs, title, preco}) {
     return (
@@ -18,8 +20,16 @@ function Cards({imgs, title, preco}) {
                 <Card.Text className='preco'> {preco}</Card.Text>
                 </div>
                 <div className='buttons'>
-                <Button variant="primary" className='text'>adicionar ao carrinho</Button>
-                <Button variant="primary" className='text'>Comprar</Button>
+                <Button variant="primary" className='text'>
+                    <span className='me-2'>
+                    <FontAwesomeIcon icon={faCartPlus}/>
+                    </span>
+                    adicionar ao carrinho</Button>
+                <Button variant="primary" className='text'>
+                    <span className='me-2'>
+                    <FontAwesomeIcon icon={faCartShopping}/>
+                    </span>
+                    Comprar</Button>
                 </div>
 
             </Card.Body>
